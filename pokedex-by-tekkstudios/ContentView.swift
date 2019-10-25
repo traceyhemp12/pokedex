3e6844c8a8b8c50ef4f116d3381887bc9d2b6103
Tracey Hemphill-Wright <tracey.hemphill-wright@odb.org>
2019-10-25 09:56:17 -0400

    Initial Commit

---
+++ pokedex-by-tekkstudios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokedex-by-tekkstudios
+//
+//  Created by Tracey Hemphill- Wright on 10/25/19.
+//  Copyright © 2019 Tracey Hemphill- Wright. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
